3bd3085a8f5d657d23dcf19d42c8e6e898f53cfd
Munotasir2762 <muntasirpranto2762@gmail.com>
2025-01-27 22:12:10 +0600

    Variable & Arithmetaic Operator

---
+++ script.js
@@ -0,0 +1,40 @@
+
+console.log("Hello everyone");
+
+
+//Summation...
+let x = 1000;
+let y = 3480;
+
+let sum = x+y;
+
+console.log(sum);
+
+
+//Subtraction...
+let number1 = 600;
+let number2 = 450;
+
+let sub = number1-number2;
+
+console.log(sub);
+
+//Multiplication...
+let digit1 = 30;
+let digit2 = 50;
+
+let mul = digit1*digit2;
+console.log(mul);
+
+//Division...
+let data1 = 60;
+let data2 = 5;
+
+let div = data1/data2;
+console.log(div);
+
+//Remainder...
+let data3 = 3;
+let data4 = 2;
+let remainder = 60%5;
+console.log(remainder);
